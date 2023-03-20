93ce9582c7cd88fe852cee4ff6b7896aaf704efc
Miilyb <cberrios195@gmail.com>
2023-03-20 18:14:39 -0300

    primero

---
+++ src/test/java/com/iniciohtml/html/HtmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iniciohtml.html;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
